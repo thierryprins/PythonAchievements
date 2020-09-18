bd0922e6613c0166d0e812819ccd4c9b5cea53ab
Thierry P <31377@ma-web.nl>
2020-09-18 12:33:00 +0200

    Dit is de opdracht voor Pythonachievements, Family Ties

---
+++ familyties.py
@@ -0,0 +1,6 @@
+variabel_naam= "Thierry Prins"
+variabel_leeftijd= 19
+variabel_plaats= "Zaandam"
+variabel_afstand= 15.4
+
+print("Mijn naam is " + variabel_naam + ", ik ben " + str(variabel_leeftijd) + " oud en ik woon in " + variabel_plaats + ". Ik woon " + str(variabel_afstand) + " kilometer van Mediacollege")
